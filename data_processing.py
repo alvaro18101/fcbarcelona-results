0097af525ab2a64e52e64a48262b7fd4853f92fc
Alvaro Siesquén <alvaro.siesquen@unmsm.edu.pe>
2024-11-27 02:01:00 -0500

    web scraping and data processing

---
+++ data_processing.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('matches.csv')
+
+main_team = 'FC Barcelona'
+
+df['Locality'] = ['local' if i == main_team else 'visiting' for i in df['Local Team']]
+
+
+df['Local Result'] = df['Result'].str.split('-').apply(lambda string: int(string[0]))
+df['Visiting Result'] = df['Result'].str.split('-').apply(lambda string: int(string[1]))
+
+status = []
+for i in range(len(df)):
+    conditions = [
+        df['Local Result'][i] > df['Visiting Result'][i] and df['Locality'][i] == 'local',
+        df['Local Result'][i] > df['Visiting Result'][i] and df['Locality'][i] == 'visiting',
+        df['Local Result'][i] < df['Visiting Result'][i] and df['Locality'][i] == 'local',
+        df['Local Result'][i] < df['Visiting Result'][i] and df['Locality'][i] == 'visiting',
+    ]
+    choices = ['Win', 'Lose', 'Lose', 'Win']
+
+    status.append(np.select(conditions, choices, default = 'Draw'))
+
+df['Status'] = status
+
+goal_difference = []
+for i in range(len(df)):
+    goal_diff = abs(df['Local Result'][i] - df['Visiting Result'][i])
+    if df['Status'][i] == 'Lose':
+        goal_diff = -goal_diff
+    goal_difference.append(goal_diff)
+
+df['Goal Difference'] = goal_difference
+
+print(df)
